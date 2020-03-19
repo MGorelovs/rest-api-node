3553a4016b3a9dadeee278c653f5cf827d213fcf
BoDoxJIeB <maksims.gorelovs@inbox.lv>
2020-03-19 16:14:38 +0200

    Added authorization check to requests

--- api/routes/servicers.js
+++ api/routes/servicers.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../queries/servicers');
+const checkAuth = require('../middleware/check-auth');
 
-router.get('/', db.getAllServicers);
-router.get('/:id', db.getSingleServicer);
+router.get('/', checkAuth, db.getAllServicers);
+router.get('/:id', checkAuth, db.getSingleServicer);
 
 module.exports = router;
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../queries/user');
+const checkAuth = require('../middleware/check-auth');
 
 router.post('/signup', db.signUp);
 router.post('/login', db.logIn);
-router.delete('/:userId', db.removeUser);
+router.delete('/:userId', checkAuth, db.removeUser);
 
 module.exports = router;
